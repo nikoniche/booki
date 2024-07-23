9633174f8c3f2a41c8a09ed622943d610fe366c0
Robin <robin.skaba@volny.cz>
2024-07-23 17:22:19 +0200

    improved book properties

--- app/src/main/java/com/example/booki/book_details_views/BookView.kt
+++ app/src/main/java/com/example/booki/book_details_views/BookView.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.example.booki.MyDivider
 import com.example.booki.Book
